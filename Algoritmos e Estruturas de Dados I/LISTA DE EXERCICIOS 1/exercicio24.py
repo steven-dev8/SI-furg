c2eb62add583066fa1a18a6ca7a5a1ded210eb15
Steven Araújo <stevenndev8@gmail.com>
2024-08-09 20:04:06 -0300

    update

--- Algoritmos e Estruturas de Dados I/LISTA DE EXERCICIOS 1/exercicio24.py
+++ Algoritmos e Estruturas de Dados I/LISTA DE EXERCICIOS 1/exercicio24.py
@@ -1,179 +1,56 @@
-nota1 = float(input('Nota1: '))
+"""Em uma competição de ginástica, cada atleta recebe votos de sete jurados. A melhor
+e a pior nota são eliminadas. A sua nota fica sendo a média dos votos restantes. Faça
+um programa em Python que receba o nome do(a) ginasta, e as notas de sua
+apresentação dadas pelos sete jurados. Ao final, informe a melhor nota, a pior nota e
+a sua média final, conforme a descrição acima informada (ou seja, retirar a melhor e
+a pior nota para calcular a média). As notas não são informadas em ordem (crescente
+ou decrescente)."""
+
+#Bloco de entradas
+nota1 = float(input('Nota1: ')) #Da input das notas de 1 a 7
+#As váriaveis nota_maior e nota_menor vai receber o valor de nota1
+nota_maior = nota1 
+nota_menor = nota1
+
 nota2 = float(input('Nota2: '))
+if nota_maior < nota2:#Se nota_maior for menor que nota2, então nota_maior receberá o valor de nota2
+    nota_maior = nota2
+if nota_menor > nota2:#Se nota_menor for menor que nota2, então nota_menor receberá o valor de nota2
+    nota_menor = nota2
+
+#Segue a sentença aplicando o bloco de condicionais da nota3 até a nota7
 nota3 = float(input('Nota3: '))
+if nota_maior < nota3:
+    nota_maior = nota3
+if nota_menor > nota3:
+    nota_menor = nota3
+
 nota4 = float(input('Nota4: '))
+if nota_maior < nota4:
+    nota_maior = nota4
+if nota_menor > nota4:
+    nota_menor = nota4
+
 nota5 = float(input('Nota5: '))
+if nota_maior < nota5:
+    nota_maior = nota5
+if nota_menor > nota5:
+    nota_menor = nota5
+
 nota6 = float(input('Nota6: '))
+if nota_maior < nota6:
+    nota_maior = nota6
+if nota_menor > nota6:
+    nota_menor = nota6
+
 nota7 = float(input('Nota7: '))
+if nota_maior < nota7:
+    nota_maior = nota7
+if nota_menor > nota7:
+    nota_menor = nota7
 
-if nota1 > nota2 and nota1 > nota3 and nota1 > nota4 and nota1 > nota5 and nota1 > nota6 and nota1 > nota7:
-    if nota2 < nota3 and nota2 < nota4 and nota2 < nota5 and nota2 < nota6 and nota2 < nota7:
-        nota_maior = nota1
-        nota_menor = nota2
-    else:
-        if nota3 < nota4 and nota3 < nota5 and nota3 < nota6 and nota3 < nota7:
-            nota_maior = nota1
-            nota_menor = nota3
-        else:
-            if nota4 < nota5 and nota4 < nota6 and nota4 < nota7:
-                nota_maior = nota1
-                nota_menor = nota4
-            else:
-                if nota5 < nota6 and nota5 < nota7:
-                    nota_maior = nota1
-                    nota_menor = nota5
-                else:
-                    if nota6 < nota7:
-                        nota_maior = nota1
-                        nota_menor = nota6
-                    else:
-                        nota_maior = nota1
-                        nota_menor = nota7
-
-if nota2 > nota1 and nota2 > nota3 and nota2 > nota4 and nota2 > nota5 and nota2 > nota6 and nota2 > nota7:
-    if nota1 < nota3 and nota1 < nota4 and nota1 < nota5 and nota1 < nota6 and nota1 < nota7:
-        nota_maior = nota2
-        nota_menor = nota1
-    else:
-        if nota3 < nota4 and nota3 < nota5 and nota3 < nota6 and nota3 < nota7:
-            nota_maior = nota2
-            nota_menor = nota3
-        else:
-            if nota4 < nota5 and nota4 < nota6 and nota4 < nota7:
-                nota_maior = nota2
-                nota_menor = nota4
-            else:
-                if nota5 < nota6 and nota5 < nota7:
-                    nota_maior = nota2
-                    nota_menor = nota5
-                else:
-                    if nota6 < nota7:
-                        nota_maior = nota2
-                        nota_menor = nota6
-                    else:
-                        nota_maior = nota2
-                        nota_menor = nota7
-
-if nota3 > nota1 and nota3 > nota2 and nota3 > nota4 and nota3 > nota5 and nota3 > nota6 and nota3 > nota7:
-    if nota1 < nota2 and nota1 < nota4 and nota1 < nota5 and nota1 < nota6 and nota1 < nota7:
-        nota_maior = nota3
-        nota_menor = nota1
-    else:
-        if nota2 < nota4 and nota2 < nota5 and nota2 < nota6 and nota2 < nota7:
-            nota_maior = nota3
-            nota_menor = nota2
-        else:
-            if nota4 < nota5 and nota4 < nota6 and nota4 < nota7:
-                nota_maior = nota3
-                nota_menor = nota4
-            else:
-                if nota5 < nota6 and nota5 < nota7:
-                    nota_maior = nota3
-                    nota_menor = nota5
-                else:
-                    if nota6 < nota7:
-                        nota_maior = nota3
-                        nota_menor = nota6
-                    else:
-                        nota_maior = nota3
-                        nota_menor = nota7
-
-if nota4 > nota1 and nota4 > nota2 and nota4 > nota3 and nota4 > nota5 and nota4 > nota6 and nota4 > nota7:
-    if nota1 < nota2 and nota1 < nota3 and nota1 < nota5 and nota1 < nota6 and nota1 < nota7:
-        nota_maior = nota4
-        nota_menor = nota1
-    else:
-        if nota2 < nota3 and nota2 < nota5 and nota2 < nota6 and nota2 < nota7:
-            nota_maior = nota4
-            nota_menor = nota2
-        else:
-            if nota3 < nota5 and nota3 < nota6 and nota3 < nota7:
-                nota_maior = nota4
-                nota_menor = nota3
-            else:
-                if nota5 < nota6 and nota5 < nota7:
-                    nota_maior = nota4
-                    nota_menor = nota5
-                else:
-                    if nota6 < nota7:
-                        nota_maior = nota4
-                        nota_menor = nota6
-                    else:
-                        nota_maior = nota4
-                        nota_menor = nota7
-
-if nota5 > nota1 and nota5 > nota2 and nota5 > nota3 and nota5 > nota4 and nota5 > nota6 and nota5 > nota7:
-    if nota1 < nota2 and nota1 < nota3 and nota1 < nota4 and nota1 < nota6 and nota1 < nota7:
-        nota_maior = nota5
-        nota_menor = nota1
-    else:
-        if nota2 < nota3 and nota2 < nota4 and nota2 < nota6 and nota2 < nota7:
-            nota_maior = nota5
-            nota_menor = nota2
-        else:
-            if nota3 < nota4 and nota3 < nota6 and nota3 < nota7:
-                nota_maior = nota5
-                nota_menor = nota3
-            else:
-                if nota4 < nota6 and nota4 < nota7:
-                    nota_maior = nota5
-                    nota_menor = nota4
-                else:
-                    if nota6 < nota7:
-                        nota_maior = nota5
-                        nota_menor = nota6
-                    else:
-                        nota_maior = nota5
-                        nota_menor = nota7
-
-if nota6 > nota1 and nota6 > nota2 and nota6 > nota3 and nota6 > nota4 and nota6 > nota5 and nota6 > nota7:
-    if nota1 < nota2 and nota1 < nota3 and nota1 < nota4 and nota1 < nota5 and nota1 < nota7:
-        nota_maior = nota6
-        nota_menor = nota1
-    else:
-        if nota2 < nota3 and nota2 < nota4 and nota2 < nota5 and nota2 < nota7:
-            nota_maior = nota6
-            nota_menor = nota2
-        else:
-            if nota3 < nota4 and nota3 < nota5 and nota3 < nota7:
-                nota_maior = nota6
-                nota_menor = nota3
-            else:
-                if nota4 < nota5 and nota4 < nota7:
-                    nota_maior = nota6
-                    nota_menor = nota4
-                else:
-                    if nota5 < nota7:
-                        nota_maior = nota6
-                        nota_menor = nota5
-                    else:
-                        nota_maior = nota6
-                        nota_menor = nota7
-
-if nota7 > nota1 and nota7 > nota2 and nota7 > nota3 and nota7 > nota4 and nota7 > nota5 and nota7 > nota6:
-    if nota1 < nota2 and nota1 < nota3 and nota1 < nota4 and nota1 < nota5 and nota1 < nota6:
-        nota_maior = nota7
-        nota_menor = nota1
-    else:
-        if nota2 < nota3 and nota2 < nota4 and nota2 < nota5 and nota2 < nota6:
-            nota_maior = nota7
-            nota_menor = nota2
-        else:
-            if nota3 < nota4 and nota3 < nota5 and nota3 < nota6:
-                nota_maior = nota7
-                nota_menor = nota3
-            else:
-                if nota4 < nota5 and nota4 < nota6:
-                    nota_maior = nota7
-                    nota_menor = nota4
-                else:
-                    if nota5 < nota6:
-                        nota_maior = nota7
-                        nota_menor = nota5
-                    else:
-                        nota_maior = nota7
-                        nota_menor = nota6
-
+#media = Soma as médias e subtrai a maior nota e a menor, depois divide por 5
 media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 - nota_maior - nota_menor) / 5
 
-print(f'A maior nota: {nota_maior:.2f}, a menor: {nota_menor:.2f} e a média: {media:.2f}')
+#Imprime a maior nota, a menor e a média
+print(f'Maior nota: {nota_maior} \nMenor nota: {nota_menor} \nMédia: {media} ')
