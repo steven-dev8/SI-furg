5fc53860b20e12ce0995a95e678fac335f006e49
Steven Araújo <stevenndev8@gmail.com>
2024-08-18 16:58:46 -0300

    update

--- Algoritmos e Estruturas de Dados I/LISTA DE EXERCICIOS 1/exercicio27.py
+++ Algoritmos e Estruturas de Dados I/LISTA DE EXERCICIOS 1/exercicio27.py
@@ -9,11 +9,11 @@ while count < qnt_vezes:
     espaço = ''
     deslocamento = 1
 
-    while deslocamento <= qnt_blocos:
+    while deslocamento < qnt_blocos:
         espaço = espaço + ' '
         deslocamento = deslocamento + 1
 
-    print(espaço,qnt_tijolo)
+    print(f'{espaço}{qnt_tijolo}')
 
     qnt_tijolo = qnt_tijolo + tijolo + tijolo
     count = count + 1
