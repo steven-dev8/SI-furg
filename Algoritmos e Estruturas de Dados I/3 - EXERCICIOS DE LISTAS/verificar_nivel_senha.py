56d6ed64ed1bd0108e8d831aaed4a67d8f8f0185
Steven Araújo <stevenndev8@gmail.com>
2024-09-06 15:53:00 -0300

    update

---
+++ Algoritmos e Estruturas de Dados I/3 - EXERCICIOS DE LISTAS/verificar_nivel_senha.py
@@ -0,0 +1,43 @@
+#Define variáveis constantes com valores booleanos
+ALFABETICO = False
+NUMERICO = False
+ESPECIAL = False
+INVALID = False
+
+#Entrada da senha
+senha = input('Digite um senha: ')
+
+#Declara uma l-string com caracteres especiais
+CARACTERES_ESPECIAIS = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"
+
+#Um loop para verificar quantos tipos de caracteres diferentes contém na senha
+for caracter in senha:
+	if (ord(caracter) >= ord('a') and ord(caracter) <= ord('z')) or (ord(caracter) >= ord('A') and ord(caracter) <= ord('Z')):
+		ALFABETICO = True
+	elif ord(caracter) >= ord('0') and ord(caracter) <= ord('9'):
+		NUMERICO = True
+	elif ord(caracter) in CARACTERES_ESPECIAIS:
+		ESPECIAL = True
+	else:
+		INVALID = True
+
+#Verifica se a senha possui caracteres inválidos
+if INVALID:
+	print('SENHA INVALIDA')
+	
+else:
+	VERIFY = [ALFABETICO, NUMERICO, ESPECIAL]
+	tipos = 0
+	
+	#Conta quantos tipos de caracteres diferentes possui a senha
+	for verificar in VERIFY:
+		if verificar:
+			tipos += 1
+			
+	#Imprime para o usuário o nível da senha
+	if tipos == 1:
+		print('SENHA FRACA')
+	elif tipos == 2:
+		print('SENHA MÉDIA')
+	else:
+		print('SENHA FORTE')
